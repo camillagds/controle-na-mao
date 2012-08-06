6fd0a29e9313f1cd48afb58f7b809b5741df635b
camillagds <camillagds@1b45ba32-510c-4363-9ae5-19a20a4252ab>
2012-08-06 03:30:24 +0000

    * XML da Tela do Exportar para Excel * Implementados os métodos de esconder teclado e ocultar campos da tela de Despesas * Colocado valor fixo pras listas de Despesas e Receitas * Inicio da implementação da Lista de Categorias

---
+++ ControleNaMao_1.0/trunk/src/controle/mao/negocio/categorias/CategoriaItemListView.java
@@ -0,0 +1,22 @@
+package controle.mao.negocio.categorias;
+
+public class CategoriaItemListView {
+
+	    private String texto;
+	    private int iconeRid;
+
+	    public CategoriaItemListView() {
+	    }
+
+	    public CategoriaItemListView(String texto) {
+	        this.texto = texto;
+	    }
+
+	    public String getTexto() {
+	        return texto;
+	    }
+
+	    public void setTexto(String texto) {
+	        this.texto = texto;
+	    }
+}
