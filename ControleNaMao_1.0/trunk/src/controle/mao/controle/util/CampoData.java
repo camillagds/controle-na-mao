b03478f711d18b77e5761e3554b3db3db86aca15
LobinhaXD <milla@silverwolf.com.br>
2012-09-30 23:06:18 +0000

    Renomeei pacote de negocio.util para controle.util

---
+++ ControleNaMao_1.0/trunk/src/controle/mao/controle/util/CampoData.java
@@ -0,0 +1,51 @@
+package controle.mao.controle.util;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+import controle.mao.R;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.DatePicker;
+
+public class CampoData extends DatePicker{
+	
+	DatePicker campoData;
+	
+    public CampoData(Context context, int campoId) {
+		super(context);
+		campoData = (DatePicker) findViewById(campoId);
+    }
+    
+    public CampoData(Context context) {
+		super(context);
+		campoData = new DatePicker(context);
+    }
+
+	/**
+	 * Método que retorna a data atual para o campo "Data do Crédito
+	 */
+	public void setDataAtual() {
+		final Calendar c = Calendar.getInstance();
+		int year = c.get(Calendar.YEAR);
+		int month = c.get(Calendar.MONTH);
+		int day = c.get(Calendar.DAY_OF_MONTH);
+
+		// set current date into datepicker
+		campoData.init(year, month, day, null);
+	}
+	
+	public Calendar getData(long data){
+		
+		Calendar calendar = Calendar.getInstance();    
+		calendar.setTimeInMillis(data);
+
+	     calendar.getTime();
+	     return calendar;
+	}
+}
