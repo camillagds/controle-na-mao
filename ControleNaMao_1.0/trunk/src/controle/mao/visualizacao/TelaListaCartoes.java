69b1c239b81ef8231d8730f744fd99bdf2f5d72e
LobinhaXD <milla@silverwolf.com.br>
2012-10-02 04:00:15 +0000

    Criação de 5 telas com conteudo estatico

--- ControleNaMao_1.0/trunk/src/controle/mao/visualizacao/TelaListaCartoes.java
+++ ControleNaMao_1.0/trunk/src/controle/mao/visualizacao/TelaListaCartoes.java
@@ -66,7 +66,7 @@ public boolean onMenuItemSelected(int featureId, MenuItem item) {
 	case BUSCAR:
 		//TODO Implementar Busca
 		// Abre a tela para buscar o carro pelo nome
-		//startActivity(new Intent(this, BuscarCarro.class));
+		//startActivity(new Intent(this, TelaBuscarFatura.class));
 		break;
 	}
 	return true;
