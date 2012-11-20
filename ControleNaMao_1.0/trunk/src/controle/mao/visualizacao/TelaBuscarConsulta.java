f59d4b9162666e1e648df8f9a055bf97445c4616
camillagds <camillagds@1b45ba32-510c-4363-9ae5-19a20a4252ab>
2012-11-20 23:36:33 +0000

    * Corrigida os botoes e spinners da tela de busca de fatura e lancamentos

--- ControleNaMao_1.0/trunk/src/controle/mao/visualizacao/TelaBuscarConsulta.java
+++ ControleNaMao_1.0/trunk/src/controle/mao/visualizacao/TelaBuscarConsulta.java
@@ -1,16 +1,23 @@
 package controle.mao.visualizacao;
 
 import controle.mao.R;
+import controle.mao.controle.cartoes.Cartao;
+import controle.mao.controle.categoria.Categoria;
+import controle.mao.controle.lancamentos.Despesa;
+import controle.mao.dados.util.CategoriasUtil;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 /**
  * Tela de Consulta de Lançamentos.
@@ -19,14 +26,41 @@ import android.widget.Toast;
  * 
  */
 public class TelaBuscarConsulta extends Activity implements OnClickListener {
+	private Categoria bdScriptCategorias;
+	private String nomeCategoriaBD;
 	@Override
 	protected void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
+	    bdScriptCategorias = new Categoria(new CategoriasUtil(this));
 
 		setContentView(R.layout.busca_consulta);
 
-//		ImageButton btBuscar = (ImageButton) findViewById(R.id.btBuscarFatura);
-//		btBuscar.setOnClickListener(this);
+		//Lista - Categorias
+        Spinner dbCategoriaDespesas = (Spinner) findViewById(R.id.dbCategoriaBuscaConsulta);
+		
+		// Lista - Categorias
+		ArrayAdapter<String> listaCategorias = new ArrayAdapter<String>
+			(this, android.R.layout.simple_spinner_item, Categoria.SpinnerCategorias());
+	
+		listaCategorias
+			.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		dbCategoriaDespesas.setAdapter(listaCategorias);
+		dbCategoriaDespesas.setOnItemSelectedListener(new OnItemSelectedListener() {
+			
+
+			public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+            	//Pega Nome Pela Posição
+            	nomeCategoriaBD = parentView.getItemAtPosition(position).toString();
+            }
+
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+		
+		ImageButton btBuscar = (ImageButton) findViewById(R.id.btBuscaConsulta);
+		btBuscar.setOnClickListener(this);
 	}
 
 	@Override
@@ -49,7 +83,21 @@ public class TelaBuscarConsulta extends Activity implements OnClickListener {
 	 */
 	public void onClick(View view) {
 
-//		DatePicker dataFatura = (DatePicker) findViewById(R.id.dtDataBuscarFatura);
+		setResult(RESULT_CANCELED);
+		// Fecha a tela
+		finish();
+		
+		Intent trocatela = new Intent(TelaBuscarConsulta.this,
+				TelaListaLancamentos.class);
+		TelaBuscarConsulta.this.startActivity(trocatela);
+		TelaBuscarConsulta.this.finish();
+	}	
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// Fecha o banco
+		Categoria.bdScript.fechar();
 	}
 
 }
